ff8c76f3eaebef541f73406a86591ac6fae60ab4
shadhin8121 <shadhinkhan011@gmail.com>
2024-04-12 16:44:17 +0600

    meh, i am done for

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,33 @@
-import React, { useContext, useEffect, useRef } from "react";
+"use client";
+
 import Countries from "@/components/country/Country";
 import { GlobalContext } from "@/context/Context";
+import { useContext, useEffect } from "react";
 
 export default function Home() {
-  const { getAllCountryInformation, allCountryData } =
+  // getting Datas from Context
+  let { getAllCountryInformation, allCountryData, setAllCountryData } =
     useContext(GlobalContext);
-  const mountedRef = useRef(false);
 
+  // useEffect hook to call getAllCountryInformation() after page reload
   useEffect(() => {
-    // Update the mountedRef when the component mounts
-    mountedRef.current = true;
-
     // Check if the component is mounted on the client-side
-    if (typeof window !== "undefined" && mountedRef.current) {
+    if (typeof window !== "undefined") {
       // Check if the page has been reloaded
       if (performance.navigation.type === 1) {
         // Page has been reloaded, so call getAllCountryInformation()
         getAllCountryInformation();
       }
     }
+  }, [getAllCountryInformation]);
 
-    // Cleanup function to update mountedRef when component unmounts
-    return () => {
-      mountedRef.current = false;
-    };
-  }, []);
+  // console.log(allCountryData);
 
   return (
     <div className="container mx-auto mt-7 capitalize grid grid-cols-1 gap-6 items-center lg:grid-cols-4 md:grid-cols-3 ">
-      {allCountryData?.map((items) => (
-        <Countries key={items.name.common} datas={items} />
-      ))}
+      {allCountryData?.map((items) => {
+        return <Countries key={items.name.common} datas={items} />;
+      })}
     </div>
   );
 }
